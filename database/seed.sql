24271a5fe6ca4028b617cd9a4ded907119768758
riilll <fahrilbelajar1@gmail.com>
2026-06-07 15:33:05 +0700

    schema dan seed

---
+++ database/seed.sql
@@ -0,0 +1,9 @@
+insert into users (nama, email)
+values
+('Fahril', 'fahril@email.com');
+
+insert into quizzes (pertanyaan, jawaban, kategori, level)
+values
+('5 + 3', '8', 'penjumlahan', 'mudah'),
+('7 + 2', '9', 'penjumlahan', 'mudah'),
+('4 x 3', '12', 'perkalian', 'sedang');
